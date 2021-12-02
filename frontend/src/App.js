f781f130aea8ffc8d286b5ccf008725bddad692d
Tony Mu <mutony@microsoft.com>
2021-12-01 22:35:02 -0800

    add front end code

---
+++ frontend/src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button, Form, Card } from 'react-bootstrap';
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      links: [],
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    var that = this;
+    fetch(`http://localhost:5000/search?query=${this.state.query}`, {
+        method: 'GET',
+        // We convert the React state to JSON and send it as the POST body
+        // body: JSON.stringify(this.state)
+      }).then(function(response) {
+        response.json().then(json => that.setState({links: json}))
+      });
+
+    event.preventDefault();
+}
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <code>FAANGMULA</code> Job Search Engine
+          </p>
+        </header>
+        <div className="App-body">
+          <Form onSubmit={this.handleSubmit} className="App-form">
+            <Form.Group controlId="query" className="App-text-input">
+              <Form.Control type="text" value={this.state.value} name="query" onChange={this.handleChange} />
+            </Form.Group>
+            <Button variant="success" type="submit" className="App-button">
+              Search
+            </Button>
+          </Form>
+          <div className="App-result">
+            {
+              this.state.links.map((link) =>
+              <Card key={link}>
+                <Card.Body><a href={link}>{link}</a></Card.Body>
+              </Card>
+              )
+            }
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+  crossorigin="anonymous"
+/>
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
